47c0b1c8c922026ca9c4cd9fdc15f81335655e06
fResult <styxmaz@gmail.com>
2024-12-03 23:30:49 +0700

    Implement Day03: Mull It Over - Part 1.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,20 +1,30 @@
 /**
- * [Day 3: xxxxx](https://adventofcode.com/2024/day/3)
+ * [Day 3: Mull It Over](https://adventofcode.com/2024/day/3)
  */
 fun main() {
   val MAIN_INPUT_FILE = "Day03_test"
-  val EXPECTED_PART_1_RESULT = 1
-  val EXPECTED_PART_2_RESULT = 1
+  val EXPECTED_PART_1_RESULT = 173529487
+  val EXPECTED_PART_2_RESULT = 6
   val SAMPLE_INPUT_FILE = "Day03_sample"
-  val EXPECTED_SAMPLE_PART_1_RESULT = 1
+  val EXPECTED_SAMPLE_PART_1_RESULT = 161
   val EXPECTED_SAMPLE_PART_2_RESULT = 1
 
-  fun part1(input: List<String>): Int {
-    return input.size
+  val times: (Int, Int) -> Int = { x, y -> x * y }
+  fun matchedResultToMultiplicationResult(matchResult: MatchResult): Int {
+    val patternToReplace = "[mul(|)]".toRegex()
+    val delimeter = ","
+    return matchResult.value.replace(patternToReplace, "").split(delimeter).mapToInts().fold(1, times)
   }
 
-  fun part2(input: List<String>): Int {
-    return input.size
+  fun part1(lines: List<String>): Int {
+    return lines.sumOf { line ->
+      val targetPattern = "mul\\([0-9]{1,3},[0-9]{1,3}\\)".toRegex()
+      targetPattern.findAll(line).map(::matchedResultToMultiplicationResult).sum()
+    }
+  }
+
+  fun part2(lines: List<String>): Int {
+    return lines.size
   }
 
   // Test if implementation meets criteria from the description (`src/Day03_sample.txt`), like:
@@ -24,16 +34,16 @@ fun main() {
   samplePart1Result.displayWith("Sample Part 1")
   samplePart2Result.displayWith("Sample Part 2")
   check(EXPECTED_SAMPLE_PART_1_RESULT == samplePart1Result)
-  check(EXPECTED_SAMPLE_PART_2_RESULT == samplePart2Result)
+//  check(EXPECTED_SAMPLE_PART_2_RESULT == samplePart2Result)
 
   // Or read a large test input from the `src/Day03_test.txt` file:
   val testInput = readInput(MAIN_INPUT_FILE)
   val part1Result = part1(testInput)
-  val part2Result = part2(testInput)
+//  val part2Result = part2(testInput)
   part1Result.displayWith("Part 1")
-  part2Result.displayWith("Part 2")
+//  part2Result.displayWith("Part 2")
   check(EXPECTED_PART_1_RESULT == part1Result)
-  check(EXPECTED_PART_2_RESULT == part2Result)
+//  check(EXPECTED_PART_2_RESULT == part2Result)
 }
 
 // Related functions goes here
