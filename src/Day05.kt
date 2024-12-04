44234b11274685cbb8101b74186809fa7166282c
fResult <styxmaz@gmail.com>
2024-12-05 02:47:24 +0700

    Prepare Day05 files.

---
+++ src/Day05.kt
@@ -0,0 +1,39 @@
+/**
+ * [Day 4: xxxxx](https://adventofcode.com/2024/day/5)
+ */
+fun main() {
+  val MAIN_INPUT_FILE = "Day05_test"
+  val EXPECTED_PART_1_RESULT = 1
+  val EXPECTED_PART_2_RESULT = 1
+  val SAMPLE_INPUT_FILE = "Day05_sample"
+  val EXPECTED_SAMPLE_PART_1_RESULT = 1
+  val EXPECTED_SAMPLE_PART_2_RESULT = 1
+
+  fun part1(input: List<String>): Int {
+    return input.size
+  }
+
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
+
+  // Test if implementation meets criteria from the description (`src/Day05_sample.txt`), like:
+  val sampleInput = readInput(SAMPLE_INPUT_FILE)
+  val samplePart1Result = part1(sampleInput)
+  val samplePart2Result = part2(sampleInput)
+  samplePart1Result.displayWith("Sample Part 1")
+  samplePart2Result.displayWith("Sample Part 2")
+  check(EXPECTED_SAMPLE_PART_1_RESULT == samplePart1Result)
+  check(EXPECTED_SAMPLE_PART_2_RESULT == samplePart2Result)
+
+  // Or read a large test input from the `src/Day05_test.txt` file:
+  val testInput = readInput(MAIN_INPUT_FILE)
+  val part1Result = part1(testInput)
+  val part2Result = part2(testInput)
+  part1Result.displayWith("Part 1")
+  part2Result.displayWith("Part 2")
+  check(EXPECTED_PART_1_RESULT == part1Result)
+  check(EXPECTED_PART_2_RESULT == part2Result)
+}
+
+// Related functions goes here
