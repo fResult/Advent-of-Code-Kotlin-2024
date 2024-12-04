c4ec50051d1753ebb9c080fa10bfc0293bea6ee4
fResult <styxmaz@gmail.com>
2024-12-05 02:48:39 +0700

    Create infix `after` and `transpose` list functions.

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,4 @@
+import java.lang.IndexOutOfBoundsException
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -20,8 +21,30 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  */
 fun Any?.displayWith(tagPrefix: String? = "Result"): Unit = println("$tagPrefix: $this")
 
+fun List<String>.unlines(): String = this.joinToString("\n")
 fun List<String>.mapToInts(): List<Int> = this.map { it.toInt() }
 fun String.words(): List<String> = this.split(" ")
+fun <T> List<List<T>>.transpose(): List<List<T>> {
+  if (this.isEmpty() || this[0].isEmpty()) return emptyList()
+  val rowCount = this.size
+  val colCounts = this.map { it.size }
+  val maxColCount = colCounts.max()
+
+  return List(maxColCount) { colIdx ->
+    List(rowCount) { rowIdx ->
+      try {
+        this[rowIdx][colIdx]
+      } catch (e: IndexOutOfBoundsException) {
+        null
+      }
+    }.filterNotNull()
+  }
+}
 
 typealias Index = Int
 typealias Predicate<T> = (T) -> Boolean
+
+infix fun <T1, T2, R> ((T2) -> R).after(f: (T1) -> T2): (T1) -> R = { x ->
+  val g = this
+  g(f(x))
+}
